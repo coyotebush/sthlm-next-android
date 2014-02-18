d03aa12d344575f52fa84ecbf04059cc64602390
Corey Ford <corey@coreyford.name>
2014-02-18 14:27:34 +0100

    Add a few sample stations

--- src/com/saboonchi/sthlmnext/NearestListFragment.java
+++ src/com/saboonchi/sthlmnext/NearestListFragment.java
@@ -5,12 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class NearestListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nearest_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_nearest_list, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, new String[] { "Kista",
+                        "Husby", "Akalla" });
+        ((ListView) view).setAdapter(adapter);
+        return view;
     }
 }
