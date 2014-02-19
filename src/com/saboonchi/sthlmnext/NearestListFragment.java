03541d267eb40444b073136048b89f421fd22d95
Corey Ford <corey@coreyford.name>
2014-02-19 19:40:10 +0100

    Simplify list views using ListFragment

--- src/com/saboonchi/sthlmnext/NearestListFragment.java
+++ src/com/saboonchi/sthlmnext/NearestListFragment.java
@@ -1,27 +1,21 @@
 package com.saboonchi.sthlmnext;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
+import android.support.v4.app.ListFragment;
 import android.widget.SimpleAdapter;
 
 import com.saboonchi.sthlmnext.provider.SampleData;
 
-public class NearestListFragment extends Fragment {
+public class NearestListFragment extends ListFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_nearest_list, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         SimpleAdapter adapter = new SimpleAdapter(getActivity(),
                 SampleData.sampleStations(), R.layout.list_item_2_column,
                 new String[] { "name", "type", "distance" },
                 new int[] { R.id.text_main, R.id.text_sub, R.id.text_right });
-        ((ListView) view).setAdapter(adapter);
-        return view;
+        setListAdapter(adapter);
     }
 
 }
