2ae509a1966c8e0e7fcc55332ea69dd8130526ea
Corey Ford <corey@coreyford.name>
2014-02-26 20:52:13 +0100

    Toggle between list/map using the menu option instead of button

--- src/com/saboonchi/sthlmnext/NearestFragment.java
+++ src/com/saboonchi/sthlmnext/NearestFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -15,9 +16,6 @@ public class NearestFragment extends Fragment {
 
     /** Whether the map (instead of list) display is visible. */
     private boolean mapDisplayed = false;
-
-    /** Button that toggles between map and list displays. */
-    private Button toggleButton;
 
     /** ViewSwitcher containing the fragments for map and list displays. */
     private ViewSwitcher viewSwitcher;
@@ -33,32 +31,32 @@ public class NearestFragment extends Fragment {
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nearest, container,
                 false);
-        toggleButton = (Button) view.findViewById(R.id.button_nearest_toggle);
         viewSwitcher = (ViewSwitcher) view.findViewById(R.id.group_nearest);
-
-        toggleButton.setOnClickListener(new OnClickListener() {
-
-            /**
-             * Called when the "Map"/"List" toggle button is pressed.
-             */
-            @Override
-            public void onClick(View v) {
-                viewSwitcher.showNext();
-                mapDisplayed = !mapDisplayed;
-                if (mapDisplayed) {
-                    toggleButton.setText(R.string.button_list);
-                } else {
-                    toggleButton.setText(R.string.button_map);
-                }
-            }
-        });
         return view;
     }
     
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.nearest, menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.action_toggle_list_map:
+            viewSwitcher.showNext();
+            mapDisplayed = !mapDisplayed;
+            item.setIcon(mapDisplayed ? R.drawable.ic_action_view_as_list : R.drawable.ic_action_map);
+            item.setTitle(mapDisplayed ? R.string.button_list : R.string.button_map);
+            return true;
+
+        case R.id.action_refresh:
+            // TODO
+            return true;
+
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 }
